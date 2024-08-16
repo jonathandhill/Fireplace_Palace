87486917bc3e204248de7316bd80786a6f8be704
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-08-16 11:08:05 +0100

    API implementation and tested review

--- src/components/Main/Reviews/ReviewsComponent.jsx
+++ src/components/Main/Reviews/ReviewsComponent.jsx
@@ -5,26 +5,36 @@ import styles from './styleReviews.module.css'
 
 export default function ReviewsComponent() {
     const [ selectedCountry, setCountry ] = useState(null);
+    const [ Review, setReview ] = useState(null);
     const [ england, setEngland ] = useState(false);
     const [ scotland, setScotland ] = useState(false);
     const [ wales, setWales ] = useState(false);
 
-function handleClick(place){
-console.log(selectedCountry);
-setCountry(place)
-        }
+    function handleClick(place){
+        console.log(selectedCountry);
+        setCountry(place)
+    }
     //    let btn = [
     //     {place:'england', text:''},
     //     {place:'england', text:''},
     //     {place:'england', text:''}
 
     //    ]
-useEffect(()=>{
-    setEngland(selectedCountry === 'England');
-    setScotland(selectedCountry === 'Scotland');
-    setWales(selectedCountry === 'Wales');
-}, [ selectedCountry])
+    useEffect(()=>{
+        if (selectedCountry) {
 
+        fetch(`https://seal-app-336e8.ondigitalocean.app/reviews?country=${selectedCountry}`)
+            .then(response => response.json())
+            .then(data => setReview(data))
+
+        setEngland(selectedCountry === 'England');
+        setScotland(selectedCountry === 'Scotland');
+        setWales(selectedCountry === 'Wales');
+    };
+        }, [ selectedCountry])
+    
+
+    console.log(Review);
   return (
     <>
 <section>
@@ -32,7 +42,7 @@ useEffect(()=>{
     <article className={styles.reviewContainer}>
         <h1>Trusted</h1>
         <p>We've got thousands of happy customers all over the UK. Choose your country to see the latest review:</p>
-        <div class={styles.buttonContainer}>
+        <div className={styles.buttonContainer}>
         
             <button onClick={()=>handleClick('England')} 
             className={`${styles.countryButtons} ${england && styles.buttonSelected}`}>England</button>
@@ -42,6 +52,9 @@ useEffect(()=>{
 
             <button onClick={()=>handleClick('Scotland')}
              className={`${styles.countryButtons} ${scotland && styles.buttonSelected}`}>Scotland</button>
+        </div>
+        <div>
+        {Review ? Review.text + Review.author + Review.location : ''}
         </div>
     </article>
 </section>
