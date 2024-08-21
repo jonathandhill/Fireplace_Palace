9bf6310e73b038260bd5ba2723ce6a27e69e3fe9
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-08-21 15:51:47 +0100

    Finished all form with UseReducer

--- src/components/Booking/BookingComponent.jsx
+++ src/components/Booking/BookingComponent.jsx
@@ -1,17 +1,26 @@
 "use client";
 import { useReducer, useState } from "react";
+
+// create initialState (object with data and errorStatus keys)
 const initialState = {
   data: {
-    fullName: "",
+    FullName: '',
+    Postcode: '',
+    House: '',
+    City: '',
+    PhoneNumber: '',
+    EmailAddress: ''
   },
   errorStatus: false,
 };
+
+// Reducer function which has data and action, and different cases of action.type
 function reducer(state, action) {
   switch (action.type) {
     case "CHANGE_FORM_DATA":
       // Make a copy of current state
       let newState = { ...state };
-
+      
       // grab the data out of your action's payload
       const fieldName = action.payload.name;
       const newFieldValue = action.payload.value;
@@ -19,6 +28,7 @@ function reducer(state, action) {
       // update the newState with the changed data
       newState.data[fieldName] = newFieldValue;
       // return new state 🔥🚀
+      
       return newState;
 
     // dont forget your breaks - important
@@ -29,11 +39,13 @@ function reducer(state, action) {
       return state;
   }
 }
+
 export default function BookingComponent() {
   // New "mocked up" state object that combines that stuff
 
   const [hasErrors, setHasErrors] = useState(false);
 
+  // passing reducer function and initialState into useReducer. That gives the Reducer function the initalState as 'state'
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleChangeEvent = (event) => {
@@ -74,21 +86,81 @@ export default function BookingComponent() {
                   Full name:
                   <input
                     type="text"
-                    name="fullName"
-                    value={state.data.fullName}
+                    name="FullName"
+                    value={state.data.FullName}
                     onChange={(event) => {
-                      handleChangeEvent(event);
+                    handleChangeEvent(event);
                     }}
                   ></input>
                 </label>
               </div>
-              <div>//next field</div>
+              <div>
+                <label>
+                  Postcode:
+                  <input 
+                  type="text" 
+                  name="Postcode" 
+                  value={state.data.PostCode} 
+                  onChange={(event) => {
+                  handleChangeEvent(event);
+                    }}></input>
+                </label>
+              </div>
             </div>
+            <div>
+                <label>
+                  House/Flat Number and Street Name:
+                  <input 
+                  type="text" 
+                  name="House" 
+                  value={state.data.House}
+                  onChange={(event) => {
+                  handleChangeEvent(event);
+                  }}></input>
+                </label>
+              </div>
+              <div>
+                <label>
+                  City:
+                  <input 
+                  type="text" 
+                  name="City"
+                  value={state.data.City}
+                  onChange={(event) => {
+                  handleChangeEvent(event);
+                  }}></input>
+                </label>
+              </div>
           </fieldset>
-
           <fieldset>
             <legend> Contact Information:</legend>
-            <div></div>
+            <div>
+              <div>
+                <label>
+                  Phone Number:
+                  <input 
+                  type="text" 
+                  name="PhoneNumber"
+                  value={state.data.PhoneNumber}
+                  onChange={(event) => {
+                  handleChangeEvent(event);
+                  }}></input>
+                </label>
+              </div>
+              <div>
+                <label>
+                  Email Address:
+                  <input 
+                  type="text" 
+                  name="EmailAddress"
+                  value={state.data.EmailAddress}
+                  onChange={(event) => {
+                  handleChangeEvent(event);
+                  }}
+                  ></input>
+                </label>
+              </div>
+            </div>
           </fieldset>
           {hasErrors && (
             <p style={{ color: "red" }}>Please fill in all fields.</p>
